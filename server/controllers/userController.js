afc8299f7ebca8ed8c4d7d3c508c4fe69634ca83
Maayan Eshco <Maayaneshcob@gmail.com>
2026-06-08 13:09:09 +0300

    Add email validation to profile update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,6 @@ const getProfile = async (req, res) => {
 
 // Update the currently logged-in user's profile
 const updateProfile = async (req, res) => {
-
   // Get the current user from MongoDB using the ID from req.user
   const user = await User.findById(req.user._id);
 
@@ -16,6 +15,17 @@ const updateProfile = async (req, res) => {
     return res.status(404).json({
       message: "User not found",
     });
+  }
+
+  // If the email was changed, check that no other user already uses it
+  if (req.body.email && req.body.email !== user.email) {
+    const emailExists = await User.findOne({ email: req.body.email });
+
+    if (emailExists) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
   }
 
   // Update fields only if new values were sent in the request body
